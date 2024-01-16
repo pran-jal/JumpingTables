53a9fc143ef8a1b92172879a9bd859ada5e5bfbf
Pranjal <72604669+pran-jal@users.noreply.github.com>
2024-01-17 00:00:40 +0530

    ADD: starting page

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Alert } from 'react-native';
 
 //local
 import { styles } from './src/styles';
+import { Table } from './src/Table';
 
 
+
+
+table = new Table()
 const App = () => {
 
     return (
             <View style={styles.container}>
-                <Text style={styles.title}>Hello World!</Text>
+                <Text style = {styles.title}>Going from</Text>
+                <Button title={table.start_from}/>
+                <Text style = {styles.title}> up to </Text>
+                <Button title={table.end_At}/>
+                {/* <Text style={styles.title}>Hello World!</Text> */}
                 <StatusBar style="auto" />
             </View>
     );
